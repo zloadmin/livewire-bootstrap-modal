919b1ead14dfc356ad6d07e999ac1778a25138f9
Vladimir Karpovich <dev.karpovich@gmail.com>
2023-04-13 15:50:03 +0530

    fork of git@github.com:aliqasemzadeh/livewire-bootstrap-modal.git

---
+++ src/Providers/LaravelLivewireModalsProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AliQasemzadeh\LaravelLivewireModals\Providers;
+
+use AliQasemzadeh\LaravelLivewireModals\Components\Modals;
+use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+
+class LaravelLivewireModalsProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'livewire-bootstrap-modal');
+
+        $this->publishes(
+            [__DIR__ . '/../../resources/views' => resource_path('views/vendor/livewire-bootstrap-modal')],
+            ['livewire-bootstrap-modal', 'livewire-bootstrap-modal:views']
+        );
+
+        Livewire::component('modals', Modals::class);
+    }
+}
